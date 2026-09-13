a213fb467035578fc2bd09cbd321c14a576903ce
Faiaz Rahman <faiazrahman332@gmail.com>
2026-09-13 22:37:29 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
